8dc81931262477a6f144bfef89662ee0a118f74b
Raven Paragas <ravenparagas@Ravens-MacBook-Pro.local>
2023-09-26 03:40:05 +0800

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { UseCallback } from './components/UseCallback'
+import { UseMemo } from './components/UseMemo'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>React + Typescript</h1>
+        // <h1>React Hooks Usage</h1>
+        // // <UseCallback />
+        // // <hr />
+        // // <UseMemo />
+      </div>
+    </>
+  )
+}
+
+export default App
